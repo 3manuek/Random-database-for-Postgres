335a04d9448cd7b3b30da7633356e3709f1f935c
Emanuel Calvo <3manuek@gmail.com>
2017-02-26 19:45:21 -0300

    Update random_words_text.sql

--- random_words_text.sql
+++ random_words_text.sql
@@ -1,7 +1,14 @@
 
+-- Copy the words text file into your data directory
+-- # cp cp /usr/share/dict/words data/pg95/words.list
+
 -- Simpler, and cleaner
 DROP TABLE randomTable;
+
 CREATE TABLE randomTable(id serial PRIMARY KEY, description text);
+CREATE INDEX ON randomTable USING GIN(to_tsvector('english',description));
+
+
 CREATE OR REPLACE FUNCTION getNArrayS(el text[], count int) RETURNS text AS $$
   SELECT string_agg(el[random()*(array_length(el,1)-1)+1], ' ') FROM generate_series(1,count) g(i)
 $$
@@ -12,4 +19,6 @@ WITH t(ray) AS(
   SELECT (string_to_array(pg_read_file('words.list')::text,E'\n')) 
 ) 
 INSERT INTO randomTable(description)
-SELECT getNArrayS(T.ray, 3) FROM T, generate_series(1,10000);
+-- For a fixed size of phrase , use getNArrayS(array, <num>)
+SELECT getNArrayS(T.ray, round(random()*5+1)::int) FROM T, generate_series(1,10000);
+
